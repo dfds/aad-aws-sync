30fea255a07543dbd7634499f50de8ea86c21cd5
Emil H. Clausen <sequoiia@hummel.yt>
2023-01-09 19:20:16 +0100

    Added graceful shutdown for background jobs

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.dfds.cloud/aad-aws-sync/internal/util"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 const TIME_FORMAT = "2006-01-02 15:04:05.999999999 -0700 MST"
@@ -14,11 +20,34 @@ func main() {
 	defer util.Logger.Sync()
 	http.Handle("/metrics", promhttp.Handler())
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	backgroundJobWg := &sync.WaitGroup{}
 	go func() {
 		util.Logger.Info("Initialising Orchestrator")
-		orc := util.NewOrchestrator()
+		orc := util.NewOrchestrator(ctx, backgroundJobWg)
 		orc.Init()
 	}()
 
-	http.ListenAndServe(":8080", nil)
+	httpServer := &http.Server{Addr: ":8080"}
+	//go func() {
+	//	if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+	//		log.Fatal(err)
+	//	}
+	//}()
+	go func() {
+		<-ctx.Done()
+		util.Logger.Info("Shutting down HTTP server")
+		if err := httpServer.Shutdown(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	backgroundJobWg.Wait()
+	util.Logger.Info("All background jobs stopped")
 }
