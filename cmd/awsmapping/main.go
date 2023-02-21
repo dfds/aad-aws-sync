ddd78ff9263dd591df672601e18fcb4f038159e7
Emil H. Clausen <sequoiia@hummel.yt>
2023-02-21 08:39:58 +0000

    Updated awsmapping cmd

--- cmd/awsmapping/main.go
+++ cmd/awsmapping/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"go.dfds.cloud/aad-aws-sync/internal/handler"
+	"go.dfds.cloud/aad-aws-sync/internal/util"
 	"log"
 )
 
 func main() {
+	util.InitializeLogger()
 	err := handler.AwsMappingHandler(context.TODO())
 	if err != nil {
 		log.Fatal(err)
